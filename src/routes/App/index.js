c42afabf39bc12d184009588b268190f65503cdb
bingqichen <bingqichen@live.cn>
2017-06-16 10:39:53 +0800

    update package

--- src/routes/App/index.js
+++ src/routes/App/index.js
@@ -31,7 +31,9 @@ class App extends Component {
         hello dva!
         {
           show ?
-            <Avatar src="//camo.githubusercontent.com/7c73f8cfbb808b9a451dac7d9ff5cbc2b4883419/68747470733a2f2f7a6f732e616c697061796f626a656374732e636f6d2f726d73706f7274616c2f70736167534356484f4b515671714e6a6a4d64662e6a7067" /> : null
+            <Avatar
+              src="//camo.githubusercontent.com/7c73f8cfbb808b9a451dac7d9ff5cbc2b4883419/68747470733a2f2f7a6f732e616c697061796f626a656374732e636f6d2f726d73706f7274616c2f70736167534356484f4b515671714e6a6a4d64662e6a7067"
+            /> : null
         }
       </div>
     );
